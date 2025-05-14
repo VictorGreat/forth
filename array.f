0867b762c0e55d444f4f99a46a883fb434c7d910
VictorGreat <30505536+VictorGreat@users.noreply.github.com>
2025-05-14 11:25:55 +0300

    Функции для работы с массивами

--- array.f
+++ array.f
@@ -1,3 +1,6 @@
+\ поиск по массиву размером CELL.
+\ Код для простейших случвев
+\
 : SEARCH-ARRAY(CELL) \ zn a len -- addr[x]==zn|0
 0 MAX
 2>R
@@ -10,4 +13,31 @@ CELL RP@ +!
 REPEAT
 RDROP RDROP 
 DROP 0
+;
+
+\ поиск по массиву с заданным размером
+\
+\
+: SEARCH-ARRAY \ zn xt step addr len -- addr[x]==zn|0
+\ xt: zn addr -- flag
+5 N>R
+BEGIN 
+1 RPICK 0 <
+1 RPICK 0 =
+OR 
+			IF 
+			NR> NDROP 0 EXIT 
+			THEN
+5 RPICK
+2 RPICK 
+4 RPICK EXECUTE
+			IF
+			2 RPICK
+			NR> NDROP EXIT
+			THEN
+
+3 RPICK 2 RPLACE +!
+3 RPICK 1 RPLACE -!
+AGAIN
+
 ;
